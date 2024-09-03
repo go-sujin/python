c0fd35c3e2518d6398619d0278cc3b6cad7b956a
go-sujin <139517901+go-sujin@users.noreply.github.com>
2024-09-03 18:00:23 +0900

    turtle로 별 만들기

---
+++ star.py
@@ -0,0 +1,25 @@
+import turtle as t
+t.shape('turtle')
+t.setup(500,500)
+
+t.fd(100)
+t.left(360/3)
+t.fd(100)
+t.left(360/3)
+t.fd(100)
+t.left(360/3)
+
+t.penup()
+t.left(90)
+t.fd(60)
+t.right(90)
+t.pendown()
+t.fd(100)
+t.right(360/3)
+t.fd(100)
+t.right(360/3)
+t.fd(100)
+
+
+
+
